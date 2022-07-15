fdc408147c435d954d9a5ecf057d85d432034b6a
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-07-15 23:48:54 +0530

    added swagger

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require("express")
-const app = express()
+
 const cors = require("cors")
 const dotenv = require("dotenv");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Inventory-app API",
+			version: "1.0.0",
+			description: "An Inventory management website which provides basic CRUD operations.",
+		},
+		servers: [
+			{
+				url: "http://localhost:5000",
+			},
+		],
+	},
+	apis: ["./routers/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+const app = express()
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 dotenv.config();
 app.use(cors());
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,57 @@
 const router = require("express").Router()
 const User = require("../models/User")
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Users:
+ *       type: object
+ *       required:
+ *         - username
+ *         - phonenumber
+ *       properties:
+ *         username:
+ *           type: text
+ *           description: The name of user
+ *         phonenumber:
+ *           type: text
+ *           description: The phonenumber of user
+ *       example:
+ *         username: Kushal
+ *         phonenumber: 9999999999
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Users
+  *   description: The APIs for managing users
+  */
+
+
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: Authenticate user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Users'
+ *     responses:
+ *       200:
+ *         description: The user was logged in successfully and user registered if not exists
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       500:
+ *         description: Some server error
+ */
 //Login User
 router.post("/login",User.loginUser)
 
