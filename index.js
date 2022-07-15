c03feebcea7d805464a4f32063a0731ff0be93b8
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-07-15 23:56:27 +0530

    added swagger

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require("express")
-const app = express()
 const cors = require("cors")
 const dotenv = require("dotenv");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Inventory-app API",
+			version: "1.0.0",
+			description: "An Inventory management website which provides basic CRUD operations.",
+		},
+		servers: [
+			{
+				url: process.env.BACKEND_URL,
+			},
+		],
+	},
+	apis: ["./routers/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+const app = express()
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 dotenv.config();
 app.use(cors());
