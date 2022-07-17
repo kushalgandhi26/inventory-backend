bbb2b93f84498b07ce2af2777150f6363219d536
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-07-17 10:29:13 +0530

    change doc route

--- swaggerRoute/swagger.js
+++ swaggerRoute/swagger.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const swaggerUI = require("swagger-ui-express");
 const swaggerdocument = require("../swagger.json")
 
-router.use("/api-docs",swaggerUI.serve)
-router.get("/api-docs",swaggerUI.setup(swaggerdocument))
+router.use("/docs",swaggerUI.serve)
+router.get("/docs",swaggerUI.setup(swaggerdocument))
 
 module.exports = router
