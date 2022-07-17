e4c7518e76c20e16feb6178361401e8f4aa28eb9
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-07-17 10:40:50 +0530

    fix error

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-var port = process.env.PORT || 8000
-app.listen(port)
+app.listen(process.env.PORT || 8000)
 
 
-// app.use("/api",require("./swaggerRoute/swagger"))
+app.use("/api",require("./swaggerRoute/swagger"))
 app.use("/product", require("./routers/productRouter"))
 app.use("/user", require("./routers/userRouter"))
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,4 @@ const User = require("../models/User")
 //Login User
 router.post("/login",User.loginUser)
 
-//Get Users
-router.get("/getUsers",User.getUsers)
-
 module.exports = router
